b769442f07dfbbe8d3d3555b8d906f965a5e2e08
benjaQm <benja.quiroga.m85@gmail.com>
2024-01-22 04:31:34 -0300

    se agrega deserializador

--- almacenamiento_datos_netatmo/servicio_almacenamiento_json/views.py
+++ almacenamiento_datos_netatmo/servicio_almacenamiento_json/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render, redirect
 import requests
+from .models import DashboardData, Device, Place, Users
 from services.netatmo_client import NetatmoClient
 
 client = NetatmoClient()
@@ -27,5 +28,63 @@ def authorize(request: HttpRequest):
 
 '''BENJA DEBE TRABAJAR EN DESERIALIZE'''
 def deserialize(request):
+    token = request.session['access_token']
 
-    pass
+    response_json = client.get_homecoach_data(token)
+
+    usuario = Users.objects.create(
+        mail = data['mail'],
+        lang = data['lang'],
+        reg_locale = data['reg_locale'],
+        country = data['country'],
+        unit = data['unit'],
+        windunit = data['windunit'],
+        pressureunit = data['pressureunit'],
+        feel_like_algo = data['feel_like_algo']
+    )
+
+
+
+    for data in response_json['body']['devices']:
+
+        lugar = Place.objects.create(
+            altitude= data['place']['altitude'],
+            country = data['place']['country'],
+            timezone= data['place']['timezone'],
+            location= ,#transformr a texto
+        )
+
+        dispositivo = Device.objects.create(
+            user = usuario,
+            place = lugar,
+            _id = data['_id'],
+            date_setup = data['date_setup'],
+            last_setup = data['last_setup'],
+            type_ = data['type'],
+            last_status_store = data['last_status_store'],
+            firmware = data['firmware'],
+            last_upgrade = data[''],
+            wifi_status = data['wifi_status'],
+            reachable = data['reachable'],
+            co2_calibrating = data['co2_calibrating'],
+            station_name = data['station_name'],
+            read_only = True,
+            data_type = ,#transformar texto
+            subtype = data['subtype'],
+        )
+
+
+        DashboardData.objects.create(
+            data_id = data['_id'],
+            device = dispositivo,
+            time_utc = data['time_utc'],
+            temperature = data['Temperature'],
+            co2 = data['CO2'],
+            humidity = data['Humidity'],
+            noise = data['Noise'],
+            pressure = data['Pressure'],
+            absolutePressure = data['AbsolutePressure'],
+            health_idx = data['health_idx']
+        )
+
+
