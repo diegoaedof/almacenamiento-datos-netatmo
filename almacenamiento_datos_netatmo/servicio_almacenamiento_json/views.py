aa2d1468b2387f4b8df65a8096c17b3c6a35a31a
Mario <nanoantillanca@gmail.com>
2024-01-27 21:14:47 -0300

    modificacion de views.py 2

--- almacenamiento_datos_netatmo/servicio_almacenamiento_json/views.py
+++ almacenamiento_datos_netatmo/servicio_almacenamiento_json/views.py
@@ -38,23 +38,6 @@ def deserialize(request):
     token = request.session['access_token']
 
     response_json = client.get_homecoach_data(token)
-<<<<<<< HEAD
-        
-    try:
-        usuario = Users.objects.get(mail=response_json['body']['user']['mail'])
-    except Users.DoesNotExist:
-        usuario = Users.objects.create(
-            mail = response_json['body']['user']['mail'],
-            lang = response_json['body']['user']['administrative']['lang'],
-            reg_locale = response_json['body']['user']['administrative']['reg_locale'],
-            country = response_json['body']['user']['administrative']['country'],
-            unit = response_json['body']['user']['administrative']['unit'],
-            windunit = response_json['body']['user']['administrative']['windunit'],
-            pressureunit = response_json['body']['user']['administrative']['pressureunit'],
-            feel_like_algo = response_json['body']['user']['administrative']['feel_like_algo']
-        )
-
-=======
 
     usuario_data = response_json['body']['user']['administrative']
 
@@ -70,7 +53,6 @@ def deserialize(request):
             'feel_like_algo': usuario_data['feel_like_algo'],
         }
     )
->>>>>>> mario
 
     for data in response_json['body']['devices']:
         lugar_data = data['place']
@@ -81,53 +63,6 @@ def deserialize(request):
             location=str(lugar_data['location']),
         )
 
-<<<<<<< HEAD
-        try: 
-            dispositivo = Device.objects.get(_id=data['_id'])
-
-            lugar = Place.objects.create(
-                altitude= data['place']['altitude'],
-                country = data['place']['country'],
-                timezone= data['place']['timezone'],
-                location= str(data['place']['location']),
-            )
-        except Device.DoesNotExist:
-    
-            dispositivo = Device.objects.create(
-                user = usuario,
-                place = lugar,
-                _id = data['_id'],
-                date_setup = data['date_setup'],
-                last_setup = data['last_setup'],
-                device_type= data['type'],
-                last_status_store = data['last_status_store'],
-                firmware = data['firmware'],
-                last_upgrade = data['wifi_status'],
-                wifi_status = data['wifi_status'],
-                reachable = data['reachable'],
-                co2_calibrating = data['co2_calibrating'],
-                station_name = data['station_name'],
-                read_only = True,
-                data_type = str(data['data_type']),
-                subtype = data['subtype'],
-                )
-
-        if DashboardData.DoesNotExist:
-            return HttpResponse('Dispocitivo no existe')
-        else:
-            DashboardData.objects.create(
-                device = dispositivo,
-                time_utc = data['dashboard_data']['time_utc'],
-                temperature = data['dashboard_data']['Temperature'],
-                co2 = data['dashboard_data']['CO2'],
-                humidity = data['dashboard_data']['Humidity'],
-                noise = data['dashboard_data']['Noise'],
-                pressure = data['dashboard_data']['Pressure'],
-                absolutePressure = data['dashboard_data']['AbsolutePressure'],
-                health_idx = data['dashboard_data']['health_idx']
-            )
-
-=======
         dispositivo, created = Device.objects.get_or_create(
             _id=data['_id'],
             defaults={
@@ -177,6 +112,5 @@ def deserialize(request):
                     health_idx=dashboard_data.get('health_idx')
                 )
 
->>>>>>> mario
     return HttpResponse('datos almacenados')
 
